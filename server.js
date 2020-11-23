31c878c3555d2e21b40f820188c8a56c91a84873
delyan <delyan@smakmedia.com>
2020-11-23 23:29:34 +0200

    auth.rewriter sorted by permission rules

--- server.js
+++ server.js
@@ -8,16 +8,15 @@ const port = process.env.PORT || 3002;
 
 const rules = auth.rewriter({
     // Permission rules
+    index:640,
     vrscans: 640,
     materials: 640,
     colors: 640,
     tags: 640,
-
     industries: 640,
     manufacturers: 640,
 
-    favorites: 660,
-    index:640
+    favorites: 660
 })
 
 server.db = router.db;
