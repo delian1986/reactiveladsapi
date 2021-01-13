6978d280f95e48ac115cd6b7a6b5729ae65c7864
Radko Stanev <49035696+radkostanev@users.noreply.github.com>
2021-01-13 22:34:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,21 @@ const path = require('path');
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router(path.join(__dirname,'db.json'));
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3002;
 
 const rules = auth.rewriter({
     // Permission rules
-    vrscans: 664,
-    materials: 664,
-    colors: 664,
-    tags: 664,
+    index:640,
+    vrscans: 640,
+    materials: 640,
+    colors: 640,
+    tags: 640,
+    industries: 640,
+    manufacturers: 640,
 
-    industries: 664,
-    manufacturers: 664,
-
-    favorites: 664,
-    index: 664
+    favorites: 660
 })
 
 server.db = router.db;
@@ -27,7 +26,7 @@ server.use(rules)
 server.use(auth);
 
 server.get('/index', (req, res) => {
-    const { userId, vrscansLimit } = req.query;
+    const {email, vrscansLimit} = req.query;
 
     const data = {
         'materials': server.db.get('materials'),
@@ -39,8 +38,12 @@ server.get('/index', (req, res) => {
         data.vrscans = server.db.get('vrscans').slice(0, vrscansLimit);
     }
 
-    if (userId) {
-        data.favorites = server.db.get('favorites').filter(fav => fav.userId === parseInt(userId));
+    if (email) {
+        const user = server.db.get('users').find({ email: email }).value();
+
+        if (user) {
+            data.favorites = server.db.get('favorites').filter(fav => fav.userId === user.id);
+        }
     }
 
     res.jsonp(data);
